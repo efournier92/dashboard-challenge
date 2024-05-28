63b5c04b4176f1ec5d1e088fdfcf0ee193445a89
efournier92 <efournier92@gmail.com>
2024-05-28 15:41:24 -0400

    Prove concept of importing Playbook components - Import LineGraph to `App.tsx` (pending component abstraction) - Add `bundler 2.3.14` to `.tool-versions` to satisfy requirements from README

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,21 +1,49 @@
-import React from "react"
+import React from 'react';
 
-import { CircleIconButton, Title } from "playbook-ui"
+import { LineGraph } from 'playbook-ui';
 
-import WebpackerReact from 'webpacker-react'
+import WebpackerReact from 'webpacker-react';
+
+const data = [
+  {
+    name: 'Installation',
+    data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+  },
+];
 
 const App = () => {
   return (
-    <div>
-      <CircleIconButton
-          icon="plus"
-          variant="primary"
-      />
-      <Title text="Welcome to Playbook" />
-    </div>
-  )
-}
+    <>
+      <div>
+        {/* TODO: Abstract to a dedicated component */}
+        <LineGraph
+          chartData={data}
+          title="Key Performance Indicators"
+          xAxisCategories={[
+            '5/1',
+            '5/15',
+            '6/1',
+            '6/15',
+            '7/1',
+            '7/15',
+            '8/1',
+            '8/15',
+            '9/1',
+            '9/15',
+            '10/1',
+            '10/15',
+            '11/1',
+            '11/15',
+            '12/1',
+            '12/15',
+          ]}
+          yAxisMin={0}
+        />
+      </div>
+    </>
+  );
+};
 
-WebpackerReact.setup({App})
+WebpackerReact.setup({ App });
 
-export default App
+export default App;
